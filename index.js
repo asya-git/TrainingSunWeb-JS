5748366cef76fe96c53bd0cf9d60bc8500036c49
Nastya <Nastya.k_2001@mail.ru>
2023-12-01 00:30:21 +0500

    Добавить вывод списка, повесить события на кнопки
    кнопки: Добавить, Х, отмена

--- index.js
+++ index.js
@@ -81,3 +81,88 @@ const staffs = [
     }
 ]
 
+let tbody = document.getElementById("table-body");
+const thList = document.querySelector("thead > tr").childNodes;
+const btAppend = document.querySelector(".col-1 > button");
+let modal = document.getElementById("modal");
+
+
+//Подгружаем таблицу
+function updateTable(staffs) {
+    staffs.forEach((staff) => {
+        let tr = document.createElement("tr");
+
+        thList.forEach((th) => {
+            let td = document.createElement("td");
+            switch (th.textContent) {
+                case "#":
+                    td.innerHTML = staff.id;
+                    tr.appendChild(td);
+                    break;
+                case "Имя":
+                    td.innerHTML = staff.name;
+                    tr.appendChild(td);
+                    break;
+                case "Навыки":
+                    td.innerHTML = staff.skills.join(', ');
+                    tr.appendChild(td);
+                    break;
+                case "Дата":
+                    let date = new Date(staff.employment_at);
+                    td.innerHTML = date.toLocaleDateString("ru-RU");
+                    tr.appendChild(td);
+                    break;
+                case "Пол":
+                    td.innerHTML = staff.gender == "male" ? "мужской" : "женский";
+                    tr.appendChild(td);
+                    break;
+                case "Возраст":
+                    td.innerHTML = staff.age;
+                    tr.appendChild(td);
+                    break;
+                case "Зарплата":
+                    td.innerHTML =  new Intl.NumberFormat("ru-RU", {currency: 'RUB', style: 'currency'}).format(staff.salary);
+                    tr.appendChild(td);
+                    break;
+            }
+        });
+        tbody.appendChild(tr);
+    });
+}
+
+updateTable(staffs);
+
+
+//Накладываем событие на кнопку
+btAppend.addEventListener("click", () => {
+    modal.classList.add('show');
+});
+
+let idLastStaff = 8;
+
+document.addEventListener("click", function(e) 
+{
+    //скрытие формы
+    if (e.target.classList.contains("close-modal")) {
+        modal.classList.remove('show');
+    }
+
+    //Получение данных с формы
+    if (e.target.classList.contains("save-modal")) {
+        const formData = new FormData(document.forms[0]);
+        const data = Object.fromEntries(formData.entries());
+
+        //сделать нормальный id
+    
+        data.id = ++idLastStaff;
+        data.skills = data.skills.split(/,\s|,|\s/);
+
+        updateTable([data]);
+    }
+    
+    
+});
+
+
+
+
